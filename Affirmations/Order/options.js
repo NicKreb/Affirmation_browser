f44fcec73ae69c6cba4e5cd4ce412cb7514b3ada
NicKreb <nicholashstl@outlook.com>
2024-06-01 07:33:02 -0700

    On branch order_option_style  Your branch is up to date with 'origin/order_option_style'.  Changes to be committed: 	new file:   Affirmations/Order/Option1_order/onload_order.js 	modified:   Affirmations/Order/Option1_order/option1.html 	modified:   Affirmations/Order/options.js 	modified:   Affirmations/Order/order.html

--- Affirmations/Order/options.js
+++ Affirmations/Order/options.js
@@ -1,33 +1,40 @@
-let options = 0;
+//local storage
 
-function personal_option() {
-    if( options != 0){
-        options = 0
+var options = 0;
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+
+option1.onClick = function personal_option(options){
+    if( options === 1 ){
+        return options = 0, console.log(options);
+    } else { 
+        return options = 1, console.log(options)
     };
-
-    return options;
 };
 
-function giver_option(){  
-    if( options != 1){
-        options = 1
-    };
-
-    return options;
-};
-
-function order() {
-   if (options = 0){
-        document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-heart'></i>";
-        document.getElementById('item_1').innerHTML = "I am";
-        document.getElementById('item_2').innerHTML = "I can";
-    } else if (options = 1){
-        document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-hand'></i>";
-        document.getElementById('item_1').innerHTML = "You are";
-        document.getElementById('item_2').innerHTML = "You can";
+option2.onClick = function giver_option(options){ 
+    if ( options === 0){
+        return options = 1, console.log(options);
     } else {
-        console.log("options undefined")
+        return  options = 0, console.log(options);
     };
+};
 
-    return console.log(options);
- };
+let order_list = document.getElementById('order_list');
+
+order_list.onLoad = function order(options) {
+
+    if (options === 0){
+        return (document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-heart title_icon'></i>",
+        document.getElementById('item_1').innerHTML = "I am",
+        document.getElementById('item_2').innerHTML = "I can");
+     } else if (options === 1){
+        return (document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-hand title_icon'></i>",
+        document.getElementById('item_1').innerHTML = "You are",
+        document.getElementById('item_2').innerHTML = "You can");
+     } else {
+         console.log("options undefined")
+     };
+ 
+     return console.log(options);
+  };
