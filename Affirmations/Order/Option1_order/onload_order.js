94ce63f1b6f670aae18baaa493bb7ce538b11d5d
NicKreb <nicholashstl@outlook.com>
2024-06-05 05:16:06 -0700

    On branch order_option_style  Your branch is up to date with 'origin/order_option_style'.  Changes to be committed: 	modified:   Affirmations/Order/Option1_order/onload_order.js 	modified:   Affirmations/Order/options.js

--- Affirmations/Order/Option1_order/onload_order.js
+++ Affirmations/Order/Option1_order/onload_order.js
@@ -1,19 +1,21 @@
-window.onLoad = function(){
- function order() {
-    let options = localStorage.getItem('options');
+let options = localStorage.getItem('options');
 
-    if (Object.keys(localStorage())){
-        return (document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-heart title_icon'></i>",
+window.onLoad = function(){
+
+function order() {
+    if (localStorage.getItem('personal') === 'true'){
+        console.log('goodbye')
+        document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-heart title_icon'></i>",
         document.getElementById('item_1').innerHTML = "I am",
-        document.getElementById('item_2').innerHTML = "I can");
-     } else if (options.value === 1){
-        return (document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-hand title_icon'></i>",
+        document.getElementById('item_2').innerHTML = "I can"
+      } else if (localStorage.getItem('giver') === 'true'){
+         console.log('hello') 
+        document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-hand title_icon'></i>",
         document.getElementById('item_1').innerHTML = "You are",
-        document.getElementById('item_2').innerHTML = "You can");
+        document.getElementById('item_2').innerHTML = "You can"
      } else {
          console.log("options undefined")
-     };
+     }
+    };
+    order();
   }
-
-  order();
-};
--- Affirmations/Order/options.js
+++ Affirmations/Order/options.js
@@ -5,9 +5,13 @@ localStorage.setItem('options', 'all');
 let options = localStorage.getItem('options')
 
 const option1 = document.getElementById('option1').addEventListener('click', function(){
-    window.localStorage.setItem('options', 'personal');
+        localStorage.removeItem('options'),
+        localStorage.removeItem('giver');
+        window.localStorage.setItem('personal', 'true');
 });
 
 const option2 = document.getElementById('option2').addEventListener('click', function(){
-    window.localStorage.setItem('options', 'give');
+    localStorage.removeItem('options')
+    localStorage.removeItem('personal')
+    window.localStorage.setItem('giver', 'true');
 });
