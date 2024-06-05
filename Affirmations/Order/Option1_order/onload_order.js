06afae03f8e94ad5129fe6b5b2016f8f4a3df293
NicKreb <nicholashstl@outlook.com>
2024-06-05 06:43:54 -0700

    On branch order_option_style  Your branch is up to date with 'origin/order_option_style'.  Changes to be committed: 	modified:   Affirmations/Order/Option1_order/onload_order.js     modified:   Affirmations/Order/Option1_order/option1.html

--- Affirmations/Order/Option1_order/onload_order.js
+++ Affirmations/Order/Option1_order/onload_order.js
@@ -1,21 +1,17 @@
-let options = localStorage.getItem('options');
-
-window.onLoad = function(){
-
-function order() {
-    if (localStorage.getItem('personal') === 'true'){
-        console.log('goodbye')
-        document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-heart title_icon'></i>",
-        document.getElementById('item_1').innerHTML = "I am",
-        document.getElementById('item_2').innerHTML = "I can"
-      } else if (localStorage.getItem('giver') === 'true'){
-         console.log('hello') 
-        document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-hand title_icon'></i>",
-        document.getElementById('item_1').innerHTML = "You are",
-        document.getElementById('item_2').innerHTML = "You can"
-     } else {
-         console.log("options undefined")
-     }
+function onLoad(){ 
+    for (var i = 0; i < localStorage.length; i++)
+{    if (localStorage.getItem('personal') === localStorage.getItem(localStorage.key(i))){
+            return( 
+            console.log('personal'),
+            document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-heart title_icon'></i>",
+            document.getElementById('item_1').innerHTML = "I am",
+            document.getElementById('item_2').innerHTML = "I can")
+        } else if (localStorage.getItem('giver') === localStorage.getItem(localStorage.key(i))){
+            return (console.log('giver'),
+            document.getElementById('icon_wrapper').innerHTML = "<i class='fa-solid fa-hand-holding-hand title_icon'></i>",
+            document.getElementById('item_1').innerHTML = "You are",
+            document.getElementById('item_2').innerHTML = "You can")
+        } else {
+            console.log("options undefined")
+        }}
     };
-    order();
-  }
